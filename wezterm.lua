909ae6d476b665ac21dd033f0de3c648d2fe7e19
Mark <33582774+hwhang0917@users.noreply.github.com>
2025-06-26 15:51:17 +0900

    Fix window GPU glitch and font

--- wezterm.lua
+++ wezterm.lua
@@ -3,22 +3,21 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 if os.getenv("OS") == "Windows_NT" then
-    config.default_prog = { "C:/Program Files/Git/bin/bash.exe", "-i", "-l" }
-    -- config.default_prog = { "wsl", "--cd", "~" }
+    config.default_prog = { "wsl", "--cd", "~" }
 else
     config.default_prog = { "/usr/bin/zsh" }
 end
 
-config.color_scheme = "Catppuccin Mocha (Gogh)"
+config.front_end = "WebGpu"
 
+config.color_scheme = "Catppuccin Mocha (Gogh)"
 config.font = wezterm.font_with_fallback {
-    { family = "FiraCode Nerd Font", weight = "DemiBold" },
+    { family = "FiraCode Nerd Font Ret" },
     "D2Coding ligature",
     "Segoe UI Emoji",
 }
-config.font_size = 12
+config.font_size = 14
 config.window_decorations = "RESIZE"
-
 config.window_background_opacity = 0.9
 
 return config
