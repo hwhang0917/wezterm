9f7452330e80a52d4a9fe8c62b2e1c3cc6d9586e
Mark <33582774+hwhang0917@users.noreply.github.com>
2025-07-31 11:10:45 +0900

    qol patch
    - remove ligature
    - change font
    - set new tab open with Git Bash

--- wezterm.lua
+++ wezterm.lua
@@ -3,22 +3,46 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 if os.getenv("OS") == "Windows_NT" then
-    config.default_prog = { "wsl", "--cd", "~" }
+	config.default_prog = { "wsl", "--cd", "~" }
 else
-    config.default_prog = { "/usr/bin/zsh" }
+	config.default_prog = { "/usr/bin/zsh" }
 end
 
 config.front_end = "WebGpu"
 
 config.color_scheme = "Catppuccin Mocha (Gogh)"
-config.font = wezterm.font_with_fallback {
-    { family = "FiraCode Nerd Font Ret" },
-    "D2Coding ligature",
-    "Segoe UI Emoji",
-}
-config.harfbuzz_features = { 'calt = 0', 'clig = 0', 'liga = 0' }
+config.font = wezterm.font_with_fallback({
+	-- { family = "FiraCode Nerd Font Ret" },
+	{ family = "Terminess Nerd Font Mono" },
+	"D2Coding ligature",
+	"Segoe UI Emoji",
+})
+config.harfbuzz_features = { "calt = 0", "clig = 0", "liga = 0" }
 config.font_size = 14
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.9
+
+config.keys = {
+	{
+		key = "o",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action_callback(function(window, pane)
+			local overrides = window:get_config_overrides() or {}
+			if overrides.window_background_opacity == 1.0 then
+				overrides.window_background_opacity = 0.9
+			else
+				overrides.window_background_opacity = 1.0
+			end
+			window:set_config_overrides(overrides)
+		end),
+	},
+	{
+		key = "i",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SpawnCommandInNewTab({
+			args = { os.getenv("PROGRAMFILES") .. "\\Git\\bin\\bash.exe" },
+		}),
+	},
+}
 
 return config
