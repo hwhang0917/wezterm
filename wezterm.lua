42e861dc8e40a5e31807f7e6ae902dba021b18e9
Mark <33582774+hwhang0917@users.noreply.github.com>
2025-08-29 16:51:29 +0900

    Update: Change font & distinguish windows and WSL

--- wezterm.lua
+++ wezterm.lua
@@ -18,9 +18,34 @@ config.font = wezterm.font_with_fallback({
 	"Segoe UI Emoji",
 })
 config.harfbuzz_features = { "calt = 0", "clig = 0", "liga = 0" }
-config.font_size = 14
+config.font_size = 16
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.9
+config.window_background_opacity = 1
+
+local function get_process_icon(process_name)
+	if process_name then
+		local lower_name = process_name:lower()
+		if
+			lower_name:find("wsl")
+			or lower_name:find("zsh")
+			or lower_name:find("bash") and not lower_name:find("git")
+		then
+			return "󰣇  WSL" -- Arch Linux icon for WSL
+		elseif lower_name:find("git") and lower_name:find("bash") then
+			return "   Windows" -- Windows icon for Git Bash
+		end
+	end
+	return "󰣇" -- Default to Arch icon
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	-- Get the process name
+	local process_name = tab.active_pane.foreground_process_name or ""
+	local os_name = get_process_icon(process_name)
+	return {
+		{ Text = " " .. os_name },
+	}
+end)
 
 config.keys = {
 	{
